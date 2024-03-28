a7988ed4bcf37d77cc874809ce9d9793d4a82a06
Nico0223 <carlos_dominic_gonzaga@dlsu.edu.ph>
2024-03-28 23:40:09 +0800

    replaced bcrypt with bcryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const userModel = require("../database/models/User");
 const { validationResult } = require("express-validator");
 
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const userController = require("../controllers/userController");
 const { registerValidation } = require("../validators.js");
 const User = require("../database/models/User");
 
+/* This part is unnecesary
 // GET login to display login page
 router.get("/login", (req, res) => {
   res.render("login", {
@@ -13,7 +14,7 @@ router.get("/login", (req, res) => {
 // GET register to display registration page
 router.get("/register", async (req, res) => {
   console.log("Linked to auth.js");
-});
+});*/
 
 // POST methods for form submissions
 router.post("/register", registerValidation, userController.registerUser);
