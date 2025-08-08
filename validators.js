461c14b68fd643307feefd29b5ee96e40aae2ab6
Shaira Santos <shaisan1109@gmail.com>
2025-08-08 21:49:18 +0800

    Data validation (part 1)

--- validators.js
+++ validators.js
@@ -4,6 +4,13 @@ const registerValidation = [
   // Username should not be empty
   body("username").not().isEmpty().withMessage("Username is required."),
 
+  // Username must only contain alphanumeric characters / letters and numbers
+  body("username")
+    .isLength({ min: 3, max: 20 })
+    .withMessage("Username must be between 3 and 20 characters.")
+    .matches(/^[A-Za-z0-9_]$/)
+    .withMessage("Username must only contain letters, numbers, and underscores."),
+
   // Description is optional, so we'll remove the validation for it
 
   // Password needs to be min 8 chars with complexity
@@ -16,14 +23,8 @@ const registerValidation = [
     ),
 
   // Security question and answer are required
-  body("securityQuestion")
-    .not()
-    .isEmpty()
-    .withMessage("Security question is required."),
-  body("securityAnswer")
-    .not()
-    .isEmpty()
-    .withMessage("Security answer is required."),
+  body("securityQuestion").not().isEmpty().withMessage("Security question is required."),
+  body("securityAnswer").not().isEmpty().withMessage("Security answer is required."),
 ];
 
 module.exports = { registerValidation };
